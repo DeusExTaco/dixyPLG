9eb3ed798abf32f60fddf0071c98294dd2b0d3fb
DeusExTaco <31273212+DeusExTaco@users.noreply.github.com>
2017-08-26 15:44:24 -0700

    Added: Single page scraper

--- Scraper.py
+++ Scraper.py
@@ -3,12 +3,30 @@ import bs4
 
 # url = input ( "Enter a website to extract the URL's from: " )
 # page = requests.get ( "http://" + url )
-
 page = requests.get("http://www.fort42.com/gateshark/game1719/")
-onlyTable = bs4.SoupStrainer('div', style="width:753px;")
+
+
+onlyTable = bs4.SoupStrainer(style="width:753px;")
+onlyCheat = bs4.SoupStrainer(class_="arcode-float")
+
+#print("here: " + str(page.content).strip())
+
 table_soup = bs4.BeautifulSoup(page.content, 'lxml', parse_only=onlyTable)
-data = table_soup.text
-data_soup = bs4.BeautifulSoup(data, 'lxml')
-x = data_soup.text.splitlines()
+cheat_soup = bs4.BeautifulSoup(page.content, 'lxml', parse_only=onlyCheat)
+
+data_1 = table_soup.text
+data_2 = cheat_soup.prettify()
+
+data_soup_1 = bs4.BeautifulSoup(data_1, 'lxml')
+data_soup_2 = bs4.BeautifulSoup(data_2, 'lxml')
+
+x = data_soup_1.text.splitlines()
+x_2 = data_soup_2.text.splitlines()
+
 for gameName in x:
     print(gameName)
+
+for gameName_2 in x_2:
+    print(gameName_2)
+
+print("Woo Hoo!")
