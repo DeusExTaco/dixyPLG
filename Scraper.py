f7c80080a76ec95a48085314ba016e7fc0eb0c0c
DeusExTaco <31273212+DeusExTaco@users.noreply.github.com>
2017-08-26 08:17:41 -0700

    Removed unnecessary comments Renamed variable

--- Scraper.py
+++ Scraper.py
@@ -5,12 +5,10 @@ import bs4
 # page = requests.get ( "http://" + url )
 
 page = requests.get("http://www.fort42.com/gateshark/game1719/")
-# onlyTable = bs4.SoupStrainer(style="text-decoration:underline;")
 onlyTable = bs4.SoupStrainer('div', style="width:753px;")
-# onlyTable = bs4.SoupStrainer(id="ar3ds-gamedisplay")
 table_soup = bs4.BeautifulSoup(page.content, 'lxml', parse_only=onlyTable)
 data = table_soup.text
 data_soup = bs4.BeautifulSoup(data, 'lxml')
-abc = data_soup.text.splitlines()
-for gameName in abc:
+x = data_soup.text.splitlines()
+for gameName in x:
     print(gameName)
