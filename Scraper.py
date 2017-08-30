bf0cdb6fc55b402c7c5905e45559a476e54e4b55
Adam Hernandez <deus.ex.taco@gmail.com>
2017-08-29 23:59:13 -0700

    Refactoring Removed request library Added urllib3 Added capture of cheat code description

--- Scraper.py
+++ Scraper.py
@@ -1,32 +1,38 @@
-import requests
+import urllib3
 import bs4
+import re
 
-# url = input ( "Enter a website to extract the URL's from: " )
-# page = requests.get ( "http://" + url )
-page = requests.get("http://www.fort42.com/gateshark/game1719/")
+http = urllib3.PoolManager()
+r = http.request('GET', 'http://www.fort42.com/gateshark/game1719/')
 
-
+# Soup filters
 onlyTable = bs4.SoupStrainer(style="width:753px;")
 onlyCheat = bs4.SoupStrainer(class_="arcode-float")
+onlyHdr = bs4.SoupStrainer(class_="arcode-header")
 
-#print("here: " + str(page.content).strip())
+# Initial soup creation
+table_soup = bs4.BeautifulSoup(r.data, 'lxml', parse_only=onlyTable)
+cheat_soup = bs4.BeautifulSoup(r.data, 'lxml', parse_only=onlyCheat)
+hdr_soup = bs4.BeautifulSoup(r.data, 'lxml', parse_only=onlyHdr)
 
-table_soup = bs4.BeautifulSoup(page.content, 'lxml', parse_only=onlyTable)
-cheat_soup = bs4.BeautifulSoup(page.content, 'lxml', parse_only=onlyCheat)
+# Extracts title info
+titleInfo = table_soup.text
+print(titleInfo)
 
-data_1 = table_soup.text
-data_2 = cheat_soup.prettify()
+# Extracts cheat codes
+cheatData = cheat_soup.prettify()
+cheatData_soup = bs4.BeautifulSoup(cheatData, 'lxml')
+d_2 = cheatData_soup.text.splitlines()
+for cheatCodes in d_2:
+    print(cheatCodes)
 
-data_soup_1 = bs4.BeautifulSoup(data_1, 'lxml')
-data_soup_2 = bs4.BeautifulSoup(data_2, 'lxml')
+# Extracts cheat code descriptions
+cheatDesc = hdr_soup.find_all('a')
+i = 0
+while i < 20:
+    z = str(cheatDesc[i])
+    p = re.search("(?<=>)(.*)(?=<)", z)
+    print("\n" + p.group())
+    i += 2
 
-x = data_soup_1.text.splitlines()
-x_2 = data_soup_2.text.splitlines()
-
-for gameName in x:
-    print(gameName)
-
-for gameName_2 in x_2:
-    print(gameName_2)
-
-print("Woo Hoo!")
+print("\n Woo-----------------------Hoo!")
