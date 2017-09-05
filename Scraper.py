01b5ef7615f32ac070728f1cc16e39f46a284c06
Adam Hernandez <deus.ex.taco@gmail.com>
2017-09-04 21:28:42 -0700

    Tightened up Speed improvement Individual string output

--- Scraper.py
+++ Scraper.py
@@ -13,26 +13,36 @@ onlyHdr = bs4.SoupStrainer(class_="arcode-header")
 # Initial soup creation
 table_soup = bs4.BeautifulSoup(r.data, 'lxml', parse_only=onlyTable)
 cheat_soup = bs4.BeautifulSoup(r.data, 'lxml', parse_only=onlyCheat)
-hdr_soup = bs4.BeautifulSoup(r.data, 'lxml', parse_only=onlyHdr)
+hdr_soup = bs4.BeautifulSoup(r.data, 'lxml', parse_only=onlyHdr).find_all('a')
 
-# Extracts title info
-titleInfo = table_soup.text
+# Extracts title info into a | delimited string
+titleInfo = table_soup.text \
+    .replace("Publisher: ","|") \
+    .replace("Title ID: ","|") \
+    .replace("Serial: ","|") \
+    .replace("\n", "") \
+    .strip()
 print(titleInfo)
 
-# Extracts cheat codes
-cheatData = cheat_soup.prettify()
-cheatData_soup = bs4.BeautifulSoup(cheatData, 'lxml')
-d_2 = cheatData_soup.text.splitlines()
-for cheatCodes in d_2:
-    print(cheatCodes)
+# Extracts cheat codes into a | delimited string
+cheatData = str(cheat_soup) \
+    .replace ( "<!DOCTYPE HTML>", "" )\
+    .replace ("</div>", "" ) \
+    .replace ( '<div class="arcode-float">', "" ) \
+    .replace ( "<textarea readonly=\"\">", "" ) \
+    .replace ( "</textarea>", "|") \
+    .replace ( "\r","") \
+    .replace ( "\n", " ") \
+    .rstrip ( "|" ) \
+    .strip()
+print(cheatData)
 
-# Extracts cheat code descriptions
-cheatDesc = hdr_soup.find_all('a')
+# Extracts cheat code descriptions into a | delimited string
 i = 0
+q = ""
 while i < 20:
-    z = str(cheatDesc[i])
+    z = str(hdr_soup[i])
     p = re.search("(?<=>)(.*)(?=<)", z)
-    print("\n" + p.group())
+    q += str(p.group()) + "|"
     i += 2
-
-print("\n Woo-----------------------Hoo!")
+print(q.rstrip("|"))
