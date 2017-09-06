a40e0d94c7cb6eab69bba5ba9ff886f996986ed3
Adam Hernandez <deus.ex.taco@gmail.com>
2017-09-06 11:50:48 -0700

    Code clean up Function definition

--- Scraper.py
+++ Scraper.py
@@ -1,48 +1,54 @@
-import urllib3
-import bs4
 import re
 
+import bs4
+import urllib3
+
 http = urllib3.PoolManager()
-r = http.request('GET', 'http://www.fort42.com/gateshark/game1719/')
+r = http.request('GET', 'http://www.fort42.com/gateshark/game1333/')
 
-# Soup filters
-onlyTable = bs4.SoupStrainer(style="width:753px;")
-onlyCheat = bs4.SoupStrainer(class_="arcode-float")
-onlyHdr = bs4.SoupStrainer(class_="arcode-header")
-
-# Initial soup creation
-table_soup = bs4.BeautifulSoup(r.data, 'lxml', parse_only=onlyTable)
-cheat_soup = bs4.BeautifulSoup(r.data, 'lxml', parse_only=onlyCheat)
-hdr_soup = bs4.BeautifulSoup(r.data, 'lxml', parse_only=onlyHdr).find_all('a')
 
 # Extracts title info into a | delimited string
-titleInfo = table_soup.text \
-    .replace("Publisher: ","|") \
-    .replace("Title ID: ","|") \
-    .replace("Serial: ","|") \
-    .replace("\n", "") \
-    .strip()
-print(titleInfo)
+def gettitleinfo(x):
+    onlyTable = bs4.SoupStrainer(style="width:753px;")
+    table_soup = bs4.BeautifulSoup(x.data, 'lxml', parse_only=onlyTable)
+    titleInfo = table_soup.text \
+        .replace("Publisher: ", "|") \
+        .replace("Title ID: ", "|") \
+        .replace("Serial: ", "|") \
+        .replace("\n", "") \
+        .strip()
+    o = titleInfo
+    return o
+
 
 # Extracts cheat codes into a | delimited string
-cheatData = str(cheat_soup) \
-    .replace ( "<!DOCTYPE HTML>", "" )\
-    .replace ("</div>", "" ) \
-    .replace ( '<div class="arcode-float">', "" ) \
-    .replace ( "<textarea readonly=\"\">", "" ) \
-    .replace ( "</textarea>", "|") \
-    .replace ( "\r","") \
-    .replace ( "\n", " ") \
-    .rstrip ( "|" ) \
-    .strip()
-print(cheatData)
+def getcheatcodes(x):
+    onlyCheat = bs4.SoupStrainer(class_="arcode-float")
+    cheat_soup = bs4.BeautifulSoup(x.data, 'lxml', parse_only=onlyCheat)
+    cheatData = str(cheat_soup) \
+        .replace("<!DOCTYPE HTML>", "") \
+        .replace("</div>", "") \
+        .replace('<div class="arcode-float">', "") \
+        .replace("<textarea readonly=\"\">", "") \
+        .replace("</textarea>", "|") \
+        .replace("\r", "") \
+        .replace("\n", " ") \
+        .rstrip("|") \
+        .strip()
+    o = cheatData
+    return o
+
 
 # Extracts cheat code descriptions into a | delimited string
-i = 0
-q = ""
-while i < 20:
-    z = str(hdr_soup[i])
-    p = re.search("(?<=>)(.*)(?=<)", z)
-    q += str(p.group()) + "|"
-    i += 2
-print(q.rstrip("|"))
+def getcheatdesc(x):
+    onlyHdr = bs4.SoupStrainer(class_="arcode-header")
+    hdr_soup = bs4.BeautifulSoup(x.data, 'lxml', parse_only=onlyHdr).find_all('a')
+    i = 0
+    q = ""
+    while i < 20:
+        z = str(hdr_soup[i])
+        p = re.search("(?<=>)(.*)(?=<)", z)
+        q += str(p.group()) + "|"
+        i += 2
+    o = q.rstrip("|")
+    return o
